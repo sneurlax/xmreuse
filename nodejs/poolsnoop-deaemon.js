dfba0d126769d8b6d84770cf92cb7460e23afded
sneurlax <sneurlax@gmail.com>
2018-04-08 02:06:54 -0500

    update description

--- nodejs/poolsnoop-deaemon.js
+++ nodejs/poolsnoop-deaemon.js
@@ -1,11 +1,11 @@
 /**
  * xmreuse/nodejs/poolsnoop
  * 
- * A script for scraping mining pool APIs in order to detect likely identifiable ring members using a combination of http/https requests and a Monero daemon's RPC API using Node.js
+ * A script for scraping mining pool APIs in order to detect when mining pools use one of their own coinbases as an input in one of their own transactions using a combination of http/https requests and a Monero daemon's RPC API using Node.js
  * https://github.com/sneurlax/xmreuse
  *
  * Overview:
- *  If a mining pool announces the blocks that they find, then those coinbase outputs can be associated with a particular pool.  If that output is then used in a ring signature in a block found by the same pool, then the true output in the ring is probably the coinbase.  This script scrapes mining pool APIs in order to associate coinbase outputs with a particular pool, then scans those pools' blocks to see if they've moved their own outputs.
+ *  If a mining pool announces the blocks that they find and if any of those coinbase outputs are later used in a ring signature in a transaction announced by that same pool, then the true output in that ring is probably the coinbase.  This script scrapes mining pool APIs in order to associate coinbase outputs with a particular pool, then scans those pools' transactions to see if they've moved their own outputs.
  * 
  * @author     sneurlax <sneurlax@gmail.com> (https://github.com/sneurlax)
  * @copyright  2018
